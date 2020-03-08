6999b804e929af249324da402c1837150556f86a
Charles Korn <me@charleskorn.com>
2020-03-08 17:07:30 +1100

    Add script to update reference to image in main batect repo.

---
+++ scripts/update_batect_image_ref.sh
@@ -0,0 +1,38 @@
+#! /usr/bin/env bash
+
+set -euo pipefail
+
+IMAGE_DIGEST=$(docker inspect "$IMAGE_TAG" --format '{{ index .RepoDigests 0 }}')
+WORK_DIR=$(mktemp -d)
+FILE="app/src/main/resources/cache-init-image-reference"
+
+{
+  echo "Cloning repo..."
+  git clone https://$GITHUB_USERNAME:$GITHUB_TOKEN@github.com/batect/batect.git "$WORK_DIR"
+  echo
+
+  cd "$WORK_DIR"
+
+  echo "Configuring Git..."
+  git config user.name "batect-cache-init-image pipeline"
+  git config user.email "cache-init-image-pipeline@batect.dev"
+
+  echo "Setting image reference to '$IMAGE_DIGEST'..."
+  mkdir -p "$(dirname "$FILE")"
+  echo "$IMAGE_DIGEST" > "$FILE"
+
+  echo "Preparing commit..."
+  git add "$FILE"
+
+  echo "Committing..."
+  git commit -m "Update reference to cache init image."
+
+  echo "Pushing..."
+  git show
+  #  git push
+}
+
+echo "Cleaning up..."
+rm -rf "$WORK_DIR"
+
+echo "Done!"
